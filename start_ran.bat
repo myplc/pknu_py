72878cbf739d4efd2874ae9e5f3371125ef806d1
myplc <plctohmi@gmail.com>
2025-02-06 10:24:44 +0900

    집합자료형

---
+++ start_ran.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Today's recommended menu is!!
+timeout 5 /nobreak
+python ran.py
+pause
---
+++ start_won.bat
@@ -0,0 +1,5 @@
+@echo off
+echo get dallor and won value...
+timeout 30 /nobreak
+python won.py
+pause
